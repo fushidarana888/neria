0a9324285549f941c7ebf3eca94cffb690e327bd
fushidarana888 <chektilo@mail.ru>
2026-09-18 20:20:22 +0300

    Add citizen initiatives to sections menu

--- site-header.js
+++ site-header.js
@@ -45,6 +45,7 @@
       ]],
       ['Общество',[
         ['newspaper.html','Газета','📰'],
+        ['initiatives.html','Инициативы','🛠️'],
         ['projects.html','Дочерние проекты','🧩'],
         ['community.html','Telegram','✈️']
       ]]
